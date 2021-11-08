e61a567de8d97a6b99e17152f7fd726b5d7d138d
Paserno <paserno.x@gmail.com>
2021-11-08 20:11:24 -0300

    Organizando DOM

---
+++ src/router/router.js
@@ -0,0 +1,26 @@
+const PATHS = {
+    home: {
+        path: '/',
+        template: `
+        <h1> Home </h1>
+        <p>hola wacho q tal </p>
+        <img src="htmpls://source.unsplash.com/random/500x400" alt="Random Image">
+        `
+    },
+    news: {
+        path: '',
+        template: ``
+    },
+    about: {
+        path: '',
+        template: ``
+    },
+    contact: {
+        path: '',
+        template: ``
+    },
+    sorpres: {
+        path: '',
+        template: ``
+    },
+}
