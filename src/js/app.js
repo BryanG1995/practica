b6911c7f7537b0fe6532701988438dac03e25f82
Paserno <paserno.x@gmail.com>
2021-11-16 23:55:23 -0300

    Update css y cambiar foto del gato

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,25 @@
 $("button").on("click",function(){
     $(this).prev().slideToggle(300);
   });
+  
+  
+  let i=0;
 
 function imageReplace()
 {
+  const hola = document.getElementById('btn_he');
+  const foto= ['nothehe','hehe'];
+  const srcImg= $('#changeHehe').attr('src',`../public/img/${foto[i]}.png`);
   
-    $('#changeHehe').attr('src','../public/img/nothehe.png');
-    
+if (i===0) {
+  srcImg;
+  hola.value= 'Hehe';
+  i++;
+}else{
+  srcImg;
+  hola.value= 'NotHehe';
+  i=i - 1;
+}
 }
 
- 
 
