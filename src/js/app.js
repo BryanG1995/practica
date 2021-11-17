ec0499d65f266fe499abe8bec0934f16dcfef7a5
BryanG1995 <bryan.gutierrez1995@gmail.com>
2021-11-16 21:56:13 -0300

    hehe/js
    gato hehe y prueba separar js del html

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
 $("button").on("click",function(){
     $(this).prev().slideToggle(300);
   });
+
+function imageReplace()
+{
+  
+    $('#changeHehe').attr('src','../public/img/nothehe.png');
+    
+}
+
+ 
+
